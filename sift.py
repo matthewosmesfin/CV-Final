10e4fdaafca86aa34e3bf8198a2a0ad67b625778
Matthewos Mesfin <matthewosmesfin@Matthewoss-MBP.fios-router.home>
2024-04-22 23:28:53 -0400

    sift file goes through all directories

--- sift.py
+++ sift.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import os
 import json
 import sys
+import glob
 
 input = sys.argv[1]
 
@@ -17,48 +18,68 @@ best_kp1 = None
 best_kp2 = None
 best_good = []
 
+print("Sifting through all the images...")
+for root, dirs, files in os.walk("."):
+    for directory in dirs:
+        if directory.endswith("_images"):
+            dir_path = os.path.join(root, directory)
 
-for filename in os.listdir('luke_images'): 
-    check_img = cv.imread(('luke_images/' + filename),cv.IMREAD_GRAYSCALE)
-    # Initiate SIFT detector
-    sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(query_img,None)
-    kp2, des2 = sift.detectAndCompute(check_img,None)
-    # BFMatcher with default params
-    bf = cv.BFMatcher()
-    matches = bf.knnMatch(des1,des2,k=2)
-    # Apply ratio test
-    good = []
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
-    # cv.drawMatchesKnn expects list of lists as matches.
+            image_files = glob.glob(os.path.join(dir_path, "*"))
+    
+            for image_file in image_files:
+                check_img = cv.imread((image_file),cv.IMREAD_GRAYSCALE)
+                # Initiate SIFT detector
+                sift = cv.SIFT_create()
+                # find the keypoints and descriptors with SIFT
+                kp1, des1 = sift.detectAndCompute(query_img,None)
+                kp2, des2 = sift.detectAndCompute(check_img,None)
+                # BFMatcher with default params
+                bf = cv.BFMatcher()
+                matches = bf.knnMatch(des1,des2,k=2)
+                # Apply ratio test
+                good = []
+                for m,n in matches:
+                    if m.distance < 0.75*n.distance:
+                        good.append([m])
+                # cv.drawMatchesKnn expects list of lists as matches.
 
-    if len(good) > best_matches:
-        best_matches = len(good)
-        best_match = check_img
-        best_match_filename = filename
-        best_kp1 = kp1
-        best_kp2 = kp2
-        best_good = good
+                if len(good) > best_matches:
+                    best_matches = len(good)
+                    best_match = check_img
+                    best_match_filename = image_file
+                    best_kp1 = kp1
+                    best_kp2 = kp2
+                    best_good = good
 
-f = open('data/move_data_luke.json')
-moves = json.load(f)
 
-on_hit = ''
-on_block = ''
-name = ''
+print("Reading through the json files to find match, and post the on hit and on block...")
+for json_file in os.listdir("./data"):
+    # Full path of the JSON file
+    json_file_path = os.path.join("./", json_file)
+    
+    # Process the JSON file
+    with open("./data/" + json_file, 'r') as f:
+        try:
+            data = json.load(f)
 
-for m in moves:
-    if m['file_name'] == best_match_filename:
-        on_hit = m['on_hit']
-        on_block = m['on_block']
-        name = m['name']
-        #print(m['name'])
-        #print(f"On Hit: {on_hit}    On Block: {on_block}")
-        break;
+            on_hit = ''
+            on_block = ''
+            name = ''
 
+            moves = data.get("moves", [])
+            for move in moves:
+                if move['file_name'] == best_match_filename:
+                    on_hit = move['on_hit']
+                    on_block = move['on_block']
+                    name = move['name']
+                    #print(m['name'])
+                    #print(f"On Hit: {on_hit}    On Block: {on_block}")
+                    break            
+            # Do something with the JSON data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON file '{json_file_path}': {e}")
+
+print("image found!")
 img3 = cv.drawMatchesKnn(query_img,best_kp1,best_match,best_kp2,best_good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3)
 plt.title(name)
